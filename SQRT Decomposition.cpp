f7ae114c0eb12017d199daea1da4cc4d8ab3b5b4
RAPTORf472 <34707445+RAPTORf472@users.noreply.github.com>
2023-01-20 18:32:52 +0700

    Update SQRT Decomposition.cpp

--- SQRT Decomposition.cpp
+++ SQRT Decomposition.cpp
@@ -1,6 +1,9 @@
 int n;
 
+const int N = 2e5;
 const int BLOCK_SIZE = 400;
+//2e5 : 400
+//1e5 : 300
 
 int blockRes[N / BLOCK_SIZE + 10];
 int arr[N];
@@ -12,38 +15,47 @@ void preprocess() {
     
 }
 
+void update(int u, int val) {
+    
+    int block = u / BLOCK_SIZE;
+    blockRes[block] -= arr[u];
+    arr[u] = val;
+    blockRes[block] += arr[u];
+    
+}
+
 int query(int l, int r) {
     
     int blockL = (l + BLOCK_SIZE - 1) / BLOCK_SIZE;
     int blockR = r / BLOCK_SIZE;
     
-    int sum = 0ll;
+    int ans = 0;
     
     if (blockL >= blockR) {
         
-        for (int i = l; i <= r; i++) sum += arr[i];
-        return sum;
+        for (int i = l; i <= r; i++) ans += arr[i];
+        return ans;
         
     }
     
     for (int i = blockL; i < blockR; i++) {
         
-        sum += blockRes[i];
+        ans += blockRes[i];
     
     }
     
     for (int i = l; i < blockL * BLOCK_SIZE; i++) {
         
-        sum += arr[i];
+        ans += arr[i];
         
     }
     
     for (int i = blockR * BLOCK_SIZE; i <= r; i++) {
         
-        sum += arr[i];
+        ans += arr[i];
         
     }
     
-    return sum;
+    return ans;
     
 }
