b357bb2c20cba360f09837a55ffb260feaf75c25
RAPTORf472 <34707445+RAPTORf472@users.noreply.github.com>
2023-01-31 00:00:10 +0700

    Update Sparse Table (Static RMQ).cpp

--- Data Structures/Sparse Table (Static RMQ).cpp
+++ Data Structures/Sparse Table (Static RMQ).cpp
@@ -4,22 +4,24 @@ const int LOG = 25;
 struct RMQ {
     
     int st[N][LOG];
-
+    
     void init(int *arr, int n) {
         
         for (int i = 1; i <= n; i++) st[i][0] = arr[i];
         
         for (int i = 1; i < LOG; i++)
             for (int j = 1; j + (1 << i) - 1 <= n; j++)
-                st[j][i] = min(st[j][i - 1], st[j + (1 << (i - 1))][i - 1]);
-           
+                st[j][i] = max(st[j][i - 1], st[j + (1 << (i - 1))][i - 1]);
+        
     }
     
-    int Min(int l, int r) {
+    int get(int l, int r) {
         
+        if (l > r) return -INF;
         int lg = __lg(r - l + 1);
-        return min(st[l][lg], st[r - (1 << lg) + 1][lg]);
+        return max(st[l][lg], st[r - (1 << lg) + 1][lg]);
         
     }
     
 };
+
