bd625a0b68f9247d899fcbec4d7c33e16dc3bedd
RAPTORf472 <34707445+RAPTORf472@users.noreply.github.com>
2023-04-23 12:24:54 +0700

    Update Dynamic Hull.cpp

--- Data Structures/Dynamic Hull.cpp
+++ Data Structures/Dynamic Hull.cpp
@@ -1,51 +1,48 @@
-bool queryMode;
-
-struct Line {
-    
-	mutable int a, b, leftX;
-	
-	bool operator<(const Line& o) const {
-	    
-		return queryMode ? leftX < o.leftX : a < o.a;
-		
-	}
-	
-};
- 
 struct DynamicHull : multiset<Line> {
+    
+    int div(int a, int b) {
+        
+        return a / b - ((a ^ b) < 0 && a % b);
+        
+    }
+    
+    bool bad(iterator x, iterator y) {
+        
+        if (y == end()) {
+            
+            x->rightX = LINF;
+            return false;
+            
+        }
+        
+        if (x->a == y->a) x->rightX = (x->b > y->b) ? LINF : -LINF;
+        else x->rightX = div(y->b - x->b, x->a - y->a);
+        
+        return x->rightX >= y->rightX;
+        
+    }
+    
+    void add(int a, int b) {
+        
+        auto cur = insert({a, b, 0}), pre = cur;
+        
+        while (bad(cur, next(cur))) erase(next(cur));
+        
+        if (pre != begin() && bad(--pre, cur)) bad(pre, cur = erase(cur));
+        while ((cur = pre) != begin() && (--pre)->rightX >= cur->rightX) bad(pre, erase(cur));
+        
 
-	bool bad(iterator x, iterator y) {
-	    
-		if (y == end()) { 
-		    
-		    x->leftX = INF; 
-		    return false;
-		    
-		}
-		
-		if (x->a == y->a) x->leftX = x->b > y->b ? INF : -INF;
-		else x->leftX = (y->b - x->b) / (x->a - y->a);
-		
-		return x->leftX >= y->leftX;
-		
-	}
-	
-	void add(int a, int b) {
-	    
-		auto it = insert({a, b, 0});
-
-		while (bad(it, next(it))) erase(next(it));
-		while (it != begin() && bad(prev(it), it)) bad(prev(it), erase(it));
-		
-	}
-	
-	int query(int x) {
-	    
-		assert(!empty());
-		
-		queryMode = 1; auto l = *lower_bound({0,0,x}); queryMode = 0;
-		return l.a * x + l.b;
-		
-	}
-	
+    }
+    
+    int query(int x) {
+        
+        assert(!empty());
+        queryMode = 1;
+        auto l = *lower_bound({0, 0, x});
+        queryMode = 0;
+        
+        return l.a * x + l.b;
+        
+    }
+    
 };
